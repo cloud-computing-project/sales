5895f0ee7a99b9b7ab45df2ab6ccacfa37da5c3b
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-08 03:09:20 +0100

    small change in pom in services

---
+++ services/src/main/java/rso/projects/sales/services/SalesBean.java
@@ -0,0 +1,16 @@
+package rso.projects.sales.services;
+
+import com.kumuluz.ee.rest.beans.QueryParameters;
+import com.kumuluz.ee.rest.utils.JPAUtils;
+import rso.projects.sales.Sale;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@ApplicationScoped
+public class SalesBean {
+}
