517df32fbc34476bc7041a385b77890487ac5cde
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-08 02:33:43 +0100

    added sql data and model class, travis config

--- models/src/main/java/rso/projects/sales/Sale.java
+++ models/src/main/java/rso/projects/sales/Sale.java
@@ -11,4 +11,49 @@ import java.util.Date;
         })
 @UuidGenerator(name = "idGenerator")
 public class Sale {
+
+        @Id
+        @GeneratedValue(generator = "idGenerator")
+        private String id;
+
+        @Column(name = "product_id")
+        private String productId;
+
+        @Column(name = "old_price")
+        private String oldPrice;
+
+        @Column(name = "new_price")
+        private String newPrice;
+
+        public String getId() {
+                return id;
+        }
+
+        public void setId(String id) {
+                this.id = id;
+        }
+
+        public String getProductId() {
+                return productId;
+        }
+
+        public void setProductId(String productId) {
+                this.productId = productId;
+        }
+
+        public String getOldPrice() {
+                return oldPrice;
+        }
+
+        public void setOldPrice(String oldPrice) {
+                this.oldPrice = oldPrice;
+        }
+
+        public String getNewPrice() {
+                return newPrice;
+        }
+
+        public void setNewPrice(String newPrice) {
+                this.newPrice = newPrice;
+        }
 }
