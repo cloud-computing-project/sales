098b474ec83fbcb50e3284c5df325193dcf0b68e
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-10 12:34:51 +0100

    liveness probe

--- api/src/main/java/rso/projects/sales/api/v1/configuration/RestProperties.java
+++ api/src/main/java/rso/projects/sales/api/v1/configuration/RestProperties.java
@@ -2,12 +2,14 @@ package rso.projects.sales.api.v1.configuration;
 
 import com.kumuluz.ee.configuration.cdi.ConfigBundle;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.enterprise.context.ApplicationScoped;
 
 @ConfigBundle("rest-properties")
 @ApplicationScoped
 public class RestProperties {
 
+    @JsonProperty("healthy")
     private boolean healthy;
 
     public boolean isHealthy() {
