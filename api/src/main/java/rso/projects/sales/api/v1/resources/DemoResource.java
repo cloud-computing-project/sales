e4a9b6e4eca8ac16b241145b2ec5049190564194
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-10 08:59:43 +0100

    health check 2

--- api/src/main/java/rso/projects/sales/api/v1/resources/DemoResource.java
+++ api/src/main/java/rso/projects/sales/api/v1/resources/DemoResource.java
@@ -1,7 +1,10 @@
 package rso.projects.sales.api.v1.resources;
 
 import com.kumuluz.ee.common.runtime.EeRuntime;
+import rso.projects.sales.api.v1.configuration.RestProperties;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -9,7 +12,11 @@ import javax.ws.rs.core.Response;
 @Path("demo")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@ApplicationScoped
 public class DemoResource {
+
+    @Inject
+    private RestProperties restProperties;
 
     @GET
     @Path("instanceid")
@@ -30,6 +37,13 @@ public class DemoResource {
         }
 
         return Response.status(Response.Status.OK).build();
+    }
+
+    @POST
+    @Path("healthy")
+    public Response setHealth(Boolean healthy) {
+        restProperties.setHealthy(healthy);
+        return Response.ok().build();
     }
 
     private long fibonacci(int n) {
