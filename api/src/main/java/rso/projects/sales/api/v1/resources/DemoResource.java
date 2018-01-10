65a672c205deaf726291545e76386725fde09fce
Bozen Jovanovski <bozen.jovanovski@gmail.com>
2018-01-10 08:25:53 +0100

    added health in pom

--- api/src/main/java/rso/projects/sales/api/v1/resources/DemoResource.java
+++ api/src/main/java/rso/projects/sales/api/v1/resources/DemoResource.java
@@ -1,10 +1,7 @@
 package rso.projects.sales.api.v1.resources;
 
 import com.kumuluz.ee.common.runtime.EeRuntime;
-import rso.projects.sales.api.v1.configuration.RestProperties;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,11 +9,7 @@ import javax.ws.rs.core.Response;
 @Path("demo")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@ApplicationScoped
 public class DemoResource {
-
-    @Inject
-    private RestProperties restProperties;
 
     @GET
     @Path("instanceid")
@@ -37,13 +30,6 @@ public class DemoResource {
         }
 
         return Response.status(Response.Status.OK).build();
-    }
-
-    @POST
-    @Path("healthy")
-    public Response setHealth(Boolean healthy) {
-        restProperties.setHealthy(healthy);
-        return Response.ok().build();
     }
 
     private long fibonacci(int n) {
